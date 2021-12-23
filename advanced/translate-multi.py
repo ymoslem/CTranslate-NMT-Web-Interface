5b7dd86a82635b27623bf76507159a1d28430cf6
Yasmin Moslem <48152713+ymoslem@users.noreply.github.com>
2021-12-23 16:31:14 +0000

    Update translate-multi.py

--- advanced/translate-multi.py
+++ advanced/translate-multi.py
@@ -4,54 +4,23 @@ import ctranslate2
 from nltk import sent_tokenize
 
 
-def tokenize(text, sp_source_model):
-    """Use SentencePiece model to tokenize a sentence
-
-    Args:
-        text (str or list(str)): A sentence or list of sentences to tokenize
-        sp_source_model (object): Object of SentencePieceProcessor, with the source model
-
-    Returns:
-        List of tokens of the text, or list of lists (sentences) of tokens
-    """
-
-    tokens = sp_source_model.encode(text, out_type=str)
-    return tokens
-
-
-def detokenize(text, sp_target_model):
-    """Use SentencePiece model to detokenize a sentence
-
-    Args:
-        text (str or list(str)): A sentence to or list of sentences to detokenize
-        sp_target_model (object): Object of SentencePieceProcessor, with the target model
-
-    Returns:
-        String of the detokenized text, or list of detokenized sentences
-    """
-
-    translation = sp_target_model.decode(text)
-    return translation
-
-
 def translate(source, translator, sp_source_model, sp_target_model):
     """Use CTranslate model to translate a sentence
 
     Args:
         source (str): A source sentence to translate
         translator (object): Object of Translator, with the CTranslate2 model
-        sp_source_model (str): The path to the SentencePiece source model
-        sp_target_model (str): The path to the SentencePiece target model
-        device (str): "cpu" (default) or "cuda"
+        sp_source_model (object): The path to the SentencePiece source model
+        sp_target_model (object): The path to the SentencePiece target model
     Returns:
-        Translation of the source text.
+        Translation of the source text
     """
 
     source_sentences = sent_tokenize(source)  # split sentences
-    source_tokenized = tokenize(source_sentences, sp_source_model)
+    source_tokenized = sp_source_model.encode(source_sentences, out_type=str)
     translations = translator.translate_batch(source_tokenized, replace_unknowns=True)
     translations = [translation[0]["tokens"] for translation in translations]
-    translations_detokenized = detokenize(translations, sp_target_model)
+    translations_detokenized = sp_target_model.decode(translations)
 
     return translations_detokenized
 
@@ -59,12 +28,20 @@ def translate(source, translator, sp_source_model, sp_target_model):
 # [Modify] File paths here to the CTranslate2 and SentencePiece models.
 @st.cache(allow_output_mutation=True)
 def load_models(lang_pair, device="cpu"):
+    """Load CTranslate2 model and SentencePiece models
+
+    Args:
+        lang_pair (str): Language pair to load the models for
+        device (str): "cpu" (default) or "cuda"
+    Returns:
+        Model objects of Translator and SentencePieceProcessor
+    """
     if lang_pair == "English-to-French":
         ct_model_path = "/path/to/your/ctranslate2/model/"
         sp_source_model_path = "/path/to/your/sp_source.model"
         sp_target_model_path = "/path/to/your/sp_target.model"
     elif lang_pair == "French-to-English":
-        ct_model_path =  "/path/to/your/ctranslate2/model/"
+        ct_model_path = "/path/to/your/ctranslate2/model/"
         sp_source_model_path = "/path/to/your/sp_source.model"
         sp_target_model_path = "/path/to/your/sp_target.model"
 
